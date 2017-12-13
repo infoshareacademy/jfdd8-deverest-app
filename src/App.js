d6f65209797dfbc6bfea08785fe5e9da7b870768
Liwia Topolska <liwia.topolska@gmail.com>
2017-12-13 16:18:46 +0100

    adding button to App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
+import { Button } from 'semantic-ui-react'
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,12 +8,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <SearchBar/>
-
+        <Button basic color='blue'>Blue</Button>
       </div>
     );
   }
