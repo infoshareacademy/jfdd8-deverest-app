510a7c7935fe860184afb2355496005b2be38916
Jakub Patzer <nanonoise@gmail.com>
2017-12-14 16:56:10 +0100

    add party name, guests and guest list

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import './Details.css'
-import { Button, Segment } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 class Details extends React.Component {
   render() {
     return (
-        <Button inverted color='orange'>Edit</Button>
+        <div className='main'>
+
+          <div className='edit'>
+            <Button primary>Edit</Button>
+          </div>
+
+          <div className='data'>
+            <h1>Party Name</h1>
+            <span>---Guest List---</span>
+            <ul>
+              <li>Osoba 1</li>
+              <li>Osoba 2</li>
+              <li>Osoba 3</li>
+              <li>Osoba 4</li>
+              <li>Osoba 5</li>
+            </ul>
+          </div>
+
+        </div>
 
     )
   }
