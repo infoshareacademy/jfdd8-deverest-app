952065b8dc3770f05e9fcf90a31352520f8c0bb0
Liwia Topolska <liwia.topolska@gmail.com>
2017-12-14 16:19:22 +0100

    search function prepared

--- src/EventList.js
+++ src/EventList.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 
 class EventList extends React.Component {
   render() {
     return (
       <div>
       <ul>
-        <li>Raz</li>
-        <li>Dwa</li>
-        <li>Trzy</li>
+        <li>Raz</li><Button basic color='teal'>Details</Button>
+        <li>Dwa</li><Button basic color='teal'>Details</Button>
+        <li>Trzy</li><Button basic color='teal'>Details</Button>
       </ul>
       </div>
     )
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,11 +2,32 @@ import React from 'react'
 import { Input } from 'semantic-ui-react'
 
 class SearchBar extends React.Component {
+  state ={
+    term: ''
+  }
+
+  handleTermChange = (event) => {
+    this.setState({
+      term: event.target.value
+    })
+  };
+
+  search= () => {
+    // this.onSearch(this.state.term)
+    this.setState({
+      term: ''
+    })
+  };
+
   render() {
     return (
       <div>
         <Input
-          icon={{ name: 'search', circular: true, link: true }}
+          icon={{
+            name: 'search',
+            circular: true,
+            link: true,
+            onClick:()=>console.log('hello') }}
           placeholder='Search...'
         />
       </div>
