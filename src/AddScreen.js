0505a94a4c167128d798e37028776f24f41aedc9
Rafał Penkala <raf.tricity@gmail.com>
2017-12-15 15:52:39 +0100

    name your party +  number of guests + adding guests

--- src/AddScreen.js
+++ src/AddScreen.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import {Button, Form} from 'semantic-ui-react';
+import {Button, Form, Input} from 'semantic-ui-react';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
+import './AddScreen.css'
 
 
 class AddScreen extends React.Component {
   state = {
     partyInputValue: '',
     guestsInputValue: '',
+    namesInputValue: '',
     events: [
       {
         id: 1,
@@ -35,10 +37,11 @@ class AddScreen extends React.Component {
           (biggest, next) => Math.max(biggest, next),
           0
         ) + 1,
-        content: this.state.partyInputValue + this.state.guestsInputValue
+        content: this.state.partyInputValue + this.state.guestsInputValue + this.state.namesInputValue
       }),
       partyInputValue: '',
-      guestsInputValue: ''
+      guestsInputValue: '',
+      namesInputValue: ''
     });
   };
 
@@ -60,52 +63,72 @@ class AddScreen extends React.Component {
     })
   };
 
+  handleGuestsNamesChange = event => {
+    this.setState({
+      namesInputValue: event.target.value
+    })
+  };
+
   render() {
     return (
       <div>
-
         <Form onSubmit={this.addEvent}>
-          <Form.Field>
-            <label>Name your party</label>
-            <input
-              value={this.state.partyInputValue}
-              onChange={this.handlePartyNameChange}
-              placeholder='Put your party name here...'/>
-          </Form.Field>
+          <Form.Group widths='equal'>
 
-          <Form.Field>
-            <label>Number of guests</label>
-            <input
-              value={this.state.guestsInputValue}
-              onChange={this.handleGuestsNumberChange}
-              placeholder='Put number of guests here...'/>
-          </Form.Field>
+            <Form.Field>
+              <label>Name your party</label>
+              <Input size='small'
+                     value={this.state.partyInputValue}
+                     onChange={this.handlePartyNameChange}
+                     placeholder='Write party name here...'/>
+            </Form.Field>
+
+            <Form.Field>
+              <label>Number of guests</label>
+              <Input size='small'
+                     value={this.state.guestsInputValue}
+                     onChange={this.handleGuestsNumberChange}
+                     placeholder='Write number of guests here...'/>
+            </Form.Field>
+
+            <Form.Field>
+              <label>Add your guest here</label>
+              <Input
+                    size='small'
+                    value={this.state.namesInputValue}
+                    onChange={this.handleGuestsNamesChange}
+                    placeholder='Write your guest name...'/>
+            </Form.Field>
+          </Form.Group>
+
+          <ul>
+            {
+              this.state.events.map(
+                item => (
+                  <li key={item.id}>
+                    {item.content}
+                  </li>
+                )
+              )
+            }
+          </ul>
+
+          <DatePicker
+            selected={this.state.startDate}
+            onChange={this.handleTimeChange}
+            showTimeSelect
+            timeFormat="HH:mm"
+            timeIntervals={15}
+            dateFormat="LLL"
+          /><br />
 
           <Button
-            type='submit'
-          >Submit</Button>
+            type='submit'>Submit
+          </Button>
         </Form>
 
-        <ul>
-          {
-            this.state.events.map(
-              item => (
-                <li key={item.id}>
-                  {item.content}
-                </li>
-              )
-            )
-          }
-        </ul>
 
-        <DatePicker
-          selected={this.state.startDate}
-          onChange={this.handleTimeChange}
-          showTimeSelect
-          timeFormat="HH:mm"
-          timeIntervals={15}
-          dateFormat="LLL"
-        />
+
       </div>
     )
   }
