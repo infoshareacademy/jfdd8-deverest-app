67f67b47cd327dcbba3a680ac67a56a128b88242
Rafał Penkala <raf.tricity@gmail.com>
2017-12-21 16:41:04 +0100

    adding welcome screen

---
+++ src/WelcomeScreen.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import './WelcomeScreen.css'
+
+class WelcomeScreen extends Component {
+  render() {
+    return (
+      <div className="WelcomeScreen-container">
+        <div className="WelcomeScreen-gradient">
+        <button className="WelcomeScreen-button">Username</button>
+        <button className="WelcomeScreen-button">Password</button>
+        <button className="WelcomeScreen-GetStarted">Get started</button>
+
+        <a href="#">Create account</a>
+        </div>
+      </div>
+    )
+
+  }
+}
+
+export default WelcomeScreen;
