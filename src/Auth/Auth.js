dfd6530ecfb07897aeb35e2b7e9d09c3c87e0c30
Robert Cisek <r.cisek@o2.pl>
2018-01-09 16:56:54 +0100

    Auth first step...

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -4,8 +4,7 @@ import SignInForm from "./SignInForm";
 import SignUpForm from "./SignUpForm";
 import LogOut from './LogOut';
 import WelcomeScreen from "../WelcomeScreen";
-import SignInForm from "./SignInForm";
-import SignUpForm from "./SignUpForm";
+
 
 class Auth extends Component {
   state = {
