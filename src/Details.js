15aa7bdd1d06c9770f690c9ee386e58f2a0bc883
Jakub Patzer <nanonoise@gmail.com>
2018-01-10 16:09:03 +0100

    modals and details

--- src/Details.js
+++ src/Details.js
@@ -18,7 +18,7 @@ class Details extends React.Component {
               this.props.event.start.toLocaleString()
             }</span></h2>
             <div className='modal'>
-              <Modal closeIcon closeOnDimmerClick={false} trigger={<Button color='black'>Show Guests</Button>}>
+              <Modal size='tiny' closeIcon closeOnDimmerClick={false} trigger={<Button color='black'>Show Guests</Button>}>
                   <Modal.Description>
                     <div className='modal-body'>
                       <ul>
