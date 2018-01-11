3c050da374fbeec720c845566071c0c29f0e6e05
Robert Cisek <r.cisek@o2.pl>
2018-01-11 16:00:03 +0100

    Add delete button to event list

--- src/EventList.js
+++ src/EventList.js
@@ -3,9 +3,36 @@ import React from 'react'
 import { BrowserRouter as Router, Link } from 'react-router-dom'
 import {Tab, Modal, Button, Icon, Header} from 'semantic-ui-react'
 import Details from './Details'
+import firebase from 'firebase'
 
 class EventList extends React.Component {
 
+  state = {
+    tasks: []
+  };
+
+  componentDidMount() {
+    const uid = firebase.auth().currentUser.uid;
+    firebase.database().ref('/events/' + uid).on(
+      'value',
+      snapshot => {
+        const snapshotValue = snapshot.val();
+        const tasks = Object.entries(snapshotValue || {}).map(
+          ([id, value]) => ({ id, ...value })
+        );
+
+        this.setState({
+          tasks
+        })
+      }
+    )
+  }
+
+  handleRemoveClick = event => {
+    const taskId = event.target.dataset.taskId;
+    const uid = firebase.auth().currentUser.uid;
+    firebase.database().ref('/events/' + uid + '/' + taskId).remove()
+  };
 
   render() {
     return (
@@ -18,6 +45,15 @@ class EventList extends React.Component {
               <Modal closeIcon closeOnDimmerClick={false} trigger={<Button>Details</Button>} size='small'>
                   <Details event={event}/>
               </Modal>
+              <Modal closeIcon closeOnDimmerClick={false} trigger={ <Button
+                  data-task-id={event.id}
+                  onClick={this.handleRemoveClick}
+                >
+                  DELETE
+                </Button>} size='small'>
+                  <Details event={event}/>
+              </Modal>
+
             </div>
         )}
       </ul>
