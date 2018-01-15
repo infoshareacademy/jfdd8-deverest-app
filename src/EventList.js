e6119a2580874c43cfe645b026e30274488ffc88
Liwia Topolska <liwia.topolska@gmail.com>
2018-01-15 16:07:26 +0100

    styling elevnt list

--- src/EventList.js
+++ src/EventList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { BrowserRouter as Router, Link } from 'react-router-dom'
+import {BrowserRouter as Router, Link} from 'react-router-dom'
 import {Tab, Modal, Button, Icon, Header, List, Image} from 'semantic-ui-react'
 import Details from './Details'
 import firebase from 'firebase'
@@ -18,7 +18,7 @@ class EventList extends React.Component {
       snapshot => {
         const snapshotValue = snapshot.val();
         const tasks = Object.entries(snapshotValue || {}).map(
-          ([id, value]) => ({ id, ...value })
+          ([id, value]) => ({id, ...value})
         );
 
         this.setState({
@@ -37,45 +37,24 @@ class EventList extends React.Component {
   render() {
     return (
       <div>
-      <ul>
-        {this.props.events.map(
-          event=>
-            <div key={event.id}>
+        <List divided verticalAlign='middle'>
+          {this.props.events.map(
+            event =>
+              <List.Item key={event.id}>
+                <List.Content floated='right'>
 
-              {/*<List divided verticalAlign='middle'>*/}
-                {/*<List.Item>*/}
-                  {/*<List.Content floated='right'>*/}
-                    {/*<Button>*/}
-                      {/*Add*/}
-                      {/*/!*<Modal closeIcon closeOnDimmerClick={false} trigger={<Button>Details</Button>} size='small'>*!/*/}
-                        {/*/!*<Details event={event}/>*!/*/}
-                      {/*/!*</Modal>*!/*/}
-                    {/*</Button>*/}
-                  {/*</List.Content>*/}
-                  {/*<Image avatar src='/assets/images/avatar/small/lena.png' />*/}
-                  {/*<List.Content>*/}
-                    {/*{event.title}*/}
-                  {/*</List.Content>*/}
-                {/*</List.Item>*/}
-              {/*</List>*/}
-              <li>{event.title}</li>
-              <Modal closeIcon closeOnDimmerClick={false} trigger={<Button>Details</Button>} size='small'>
-                  <Details event={event}/>
-              </Modal>
-              <Modal closeIcon closeOnDimmerClick={false} trigger={ <Button
+                  <Modal closeIcon closeOnDimmerClick={false} trigger={<Button>Details</Button>} size='small'>
+                    <Details event={event}/>
+                  </Modal>
 
-                  data-task-id={event.id}
-                  onClick={this.handleRemoveClick}
-                  icon='trash'
-
-                >
-
-                </Button>} size='small'><Details event={event}/>
-              </Modal>
-
-            </div>
-        )}
-      </ul>
+                </List.Content>
+                <Image avatar src='/assets/images/avatar/small/lena.png'/>
+                <List.Content>
+                  {event.title}
+                </List.Content>
+              </List.Item>
+          )}
+        </List>
       </div>
     )
   }
