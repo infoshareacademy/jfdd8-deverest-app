7e0ceccbc6dd4ddb927e63757935af0d9906a981
Rafał Penkala <raf.tricity@gmail.com>
2018-01-19 15:28:39 +0100

    dark version - welcome screen top gap fixed + modal details (with list of guest inside)

--- src/Details.js
+++ src/Details.js
@@ -6,64 +6,57 @@ import EditScreen from './EditScreen'
 
 class Details extends React.Component {
 
-    state = {
-          modalOpen: false
-    };
+  state = {
+    modalOpen: false
+  };
 
-    handleOpen = () => this.setState({ modalOpen: true});
+  handleOpen = () => this.setState({modalOpen: true});
 
-    handleClose = () => this.setState({ modalOpen: false });
+  handleClose = () => this.setState({modalOpen: false});
 
 
   render() {
+    console.log('guests', this.props.event.guestList);
+    for(let i = 0; i < 5; i++ ){
+      this.props.event.guestList = this.props.event.guestList.concat(this.props.event.guestList);
+
+    }
     return (
       <div className='main'>
 
-          <div className='edit'>
-              <Modal
-                  closeIcon
-                  closeOnDimmerClick={false}
-                  open={this.state.modalOpen}
-                  onClose={this.handleClose}
-                  trigger={<Button onClick={this.handleOpen}>EDIT</Button>}
-                  size='small'>
-                  <EditScreen
-                    event={this.props.event}
-                    onAddDone={this.handleClose}
-                  />
-              </Modal>
-          </div>
+        <div className='edit'>
+          <Modal
+            closeIcon
+            closeOnDimmerClick={false}
+            open={this.state.modalOpen}
+            onClose={this.handleClose}
+            trigger={<Button onClick={this.handleOpen}>EDIT</Button>}
+            size='small'>
+            <EditScreen
+              event={this.props.event}
+              onAddDone={this.handleClose}
+            />
+          </Modal>
+        </div>
 
-          {/*<EditScreen event={this.props.event}/>*/}
+        {/*<EditScreen event={this.props.event}/>*/}
 
-            <div className='data'>
+        <div className='data'>
           <h1>{this.props.event.title}</h1>
           <h2>Starts at: <span className='dateView'>
               {this.props.event.start.toLocaleString()}
             </span></h2>
 
-          <div className='modal'>
-            <Modal size='tiny' closeIcon closeOnDimmerClick={false}
-                   trigger={<Button color='black'>Show Guests</Button>}>
-              <Modal.Description>
-                <div className='modal-body'>
-                  <ol>
-                    {Object.entries(this.props.event.guestList || {}).map(
-                      ([id, value]) => ({
-                        id, ...value
-                      })
-                    ).map(
-                      guest => (
-                        <li style={{padding: '10px 0'}}>{guest.guestName}</li>
-                      )
-                    )}
-                  </ol>
-                </div>
-              </Modal.Description>
-            </Modal>
-          </div>
         </div>
 
+        <div className='data'>
+          <ol>{
+            this.props.event.guestList.map(guest => (
+              <li>{guest.guestName}</li>
+            ))
+          }
+          </ol>
+        </div>
       </div>
     )
   }
